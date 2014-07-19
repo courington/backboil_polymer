12123313d9223059c6a0af5fce026bf5bbff6bb9
courington <chase.courington@gmail.com>
2014-07-19 00:16:56 -0600

    initial commit on poc of backbone boilerplate with polymer

---
+++ app/config.js
@@ -0,0 +1,13 @@
+require.config({
+  paths: {
+    "underscore": "../bower_components/lodash/dist/lodash.underscore",
+    "lodash": "../bower_components/lodash/dist/lodash",
+    "template": "../bower_components/lodash-template-loader/loader",
+    "jquery": "../bower_components/jquery/dist/jquery",
+    "backbone": "../bower_components/backbone/backbone",
+    "PolymerGestures": "../bower_components/polymer/polymer",
+    "Platform": "../bower_components/platform/platform"
+  },
+
+  deps: ["main"]
+});
